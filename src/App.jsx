69cab93f20c6b22ec60a0dfe150a74e7f7fb7190
David Pfahler <pfahler.david@gmail.com>
2020-12-12 18:41:37 +0100

    init repo first version

---
+++ src/App.jsx
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import seedrandom from 'seedrandom'
+
+// material-ui
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+import withRoot from './withRoot';
+import { users } from './users';
+import { goodDeeds } from './GoodDeeds';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  main: {
+    padding: theme.spacing(2),
+    margin: theme.spacing(1),
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+});
+
+const Checklist = ({ items, classes }) => {
+  const listItems = items.map((item, i) => <ListItem key={i} button>
+    <ListItemIcon>
+      <CheckCircleIcon />
+    </ListItemIcon>
+    <ListItemText primary={item} />
+  </ListItem>
+  );
+  return <List component="nav" className={classes.list}>
+    {listItems}
+  </List>
+}
+
+function shuffleArray(arr, rng) {
+  let shuffled = arr;
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(rng() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
+class App extends Component {
+  state = {
+    user: "",
+    day: new Date().getDate()
+  };
+
+  handleChange = event => {
+    this.setState({ user: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { user, day } = this.state;
+    const userItems = users.map((u, i) => <MenuItem key={i} value={u}>{u}</MenuItem>);
+    const myrng = new seedrandom(user);
+    let shuffledDeeds = goodDeeds;
+    shuffleArray(shuffledDeeds, myrng);
+    const startIndex = ((day - 1) * 3) % shuffledDeeds.length;
+
+    const selection = [
+      shuffledDeeds[startIndex],
+      shuffledDeeds[startIndex + 1],
+      shuffledDeeds[startIndex + 2]];
+
+    return <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.flex}>
+            Hydro Advent Challenges
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Paper square elevation={4} className={classes.main}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <form className={classes.form}>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="user">Benutzer</InputLabel>
+                <Select
+                  value={user}
+                  displayEmpty
+                  onChange={this.handleChange}
+                  inputProps={{
+                    name: 'currentUser',
+                    id: 'user',
+                  }}
+                >
+                  {userItems}
+                </Select>
+              </FormControl>
+            </form>
+          </Grid>
+          <Grid item xs={12}>
+            {user ? <Checklist items={selection} classes={classes} /> : "Wähle deinen Namen"}
+          </Grid>
+        </Grid>
+      </Paper>
+    </div >
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(App));
